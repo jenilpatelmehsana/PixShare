6ee5612d65edadf2884943a9f57d6d5ec4896054
jenilpatelmehsana <39550612+jenilpatelmehsana@users.noreply.github.com>
2021-06-11 00:34:29 +0530

    register endpoint reformatted

--- server/routes/auth/regiseter.js
+++ server/routes/auth/regiseter.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
 const bodyParser = require('body-parser')
-const User = require('./../../model/user');
-const urlencodedbody = bodyParser.urlencoded({ extended: false });
+const User = require('./../../model/user')
+const validator = require('email-validator')
+const urlencodedbody = bodyParser.urlencoded({ extended: false })
 
 
 function generateToken() {
@@ -18,23 +19,6 @@ function generateToken() {
 
 //register endpoint methods
 
-function convertToUser(req) {
-    const x = req.body; // worse naming
-    const newUser = new User({
-        userName: x.userName,
-        email: x.email,
-        password: x.password,
-        token: generateToken()
-    })
-    if(x.phone !== undefined) {
-        newUser.phone = x.phone
-    } else {
-        newUser.phone = null
-    }
-    // console.log(newUser);
-    return newUser;
-}
-
 async function registerNewUser(newUser) {
         await newUser.save((err, adv) => {
             if(err != null) {
@@ -45,10 +29,56 @@ async function registerNewUser(newUser) {
         });
 }
 
-router.post('/register', urlencodedbody, (req, res) => {
-    const newUser = convertToUser(req);
-    registerNewUser(newUser);
-    res.send('done')
+router.post('/register', urlencodedbody, async (req, res) => {
+
+    // converting to User
+    const body = req.body;
+    var newUser = new User({
+        userName: "",
+        email: "",
+        password: "",
+        token: generateToken()
+    })
+    const email = body.email;
+    if(validator.validate(email) === false) {
+        res.json({
+            success: false,
+            error: "invalid email",
+            user: null
+        })
+        return;
+    }
+
+    const password = body.password;
+    const userName = body.userName;
+
+    newUser.email = email;
+    newUser.password = password;
+    newUser.userName = userName;
+    if(body.phone !== undefined) {
+        newUser.phone = body.phone
+    }
+
+    // saving new user to the database
+
+    // registerNewUser(newUser);
+
+    var user;
+    await newUser.save().then((savedUser) => {
+        res.json({
+            success: true,
+            error: null,
+            user: savedUser
+        })
+        return;
+    }).catch((err) => {
+        res.send({
+            success: false,
+            error: "email already exist" + err.code,
+            user: null
+        })
+        return;
+    })
 })
 
 module.exports.register = router;
